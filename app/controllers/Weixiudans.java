79842c0a93df40b168bb4040d451d42d22c09fcc
cmingxu <cming.xu@gmail.com>
2013-09-02 10:07:07 +0800

    lwc

--- app/controllers/Weixiudans.java
+++ app/controllers/Weixiudans.java
@@ -57,6 +57,8 @@ public class Weixiudans extends Application {
 
         weixiudan.单据编号前缀 = "WX";
         weixiudan.单据编号 = String.format("WX%010d", Integer.parseInt(weixiudan.单据编号序号));
+
+        System.out.println(weixiudan.单据编号);
         weixiudan.创建人 = params.get("mBaoxiuren");
         weixiudan.创建日期 = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(new Date());;
         weixiudan.来源 = "PDA";
