4d5d226d31633e04e7815319549134375612d8f4
cmingxu <cming.xu@gmail.com>
2013-12-22 19:27:20 +0800

    xunjiandanshijian

--- app/controllers/Xunjiandans.java
+++ app/controllers/Xunjiandans.java
@@ -16,8 +16,12 @@ import java.util.List;
 public class Xunjiandans extends Application {
     public static void index(){
         System.out.println(params.get("before"));
-        System.out.println( Xunjiandan.find("是否完成=0 and 计划起始时间 < '" + params.get("before") + "'").toString());
-        List<Xunjiandan> xunjiandans = Xunjiandan.find("是否完成=0 and 计划起始时间 < '" + params.get("before") + "' and 巡检人='" + current_user + "'").fetch();
+
+        String nowString = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+        System.out.println(Xunjiandan.find("是否完成=0 and (计划起始时间 < '" + params.get("before") + "' and 计划起始时间 > '" + nowString +
+                "') and 巡检人='" + current_user + "'").sq);
+        List<Xunjiandan> xunjiandans = Xunjiandan.find("是否完成=0 and (计划起始时间 < '" + params.get("before") + "' and 计划起始时间 > '" + nowString +
+                "') and 巡检人='" + current_user + "'").fetch();
 //        List<Xunjiandan> xunjiandans = Xunjiandan.find("是否完成=0 and 计划起始时间 < '" + params.get("before") + "'").fetch();
 
         renderJSON(xunjiandans);
